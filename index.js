43192b11835b04fbc8927aec677eda86410ef7e1
geisondossantos <geison_bf@hotmail.com>
2022-07-29 14:42:56 -0300

    all-features

--- index.js
+++ index.js
@@ -1,72 +1,105 @@
-'use strict';
+window.addEventListener('load', () => {
+	todos = JSON.parse(localStorage.getItem('todos')) || [];
+	const newTodoForm = document.querySelector('#taskAdd');
+    const inputTask = document.querySelector('#input-taskName');
 
-let banco = [];
+	newTodoForm.addEventListener('submit', e => {
+		e.preventDefault();
 
-const getBanco = () => JSON.parse(localStorage.getItem ('todo-lists')) ?? [];
-const setBanco = (banco) => localStorage.setItem ('todo-lists', JSON.stringify(banco));
 
-const criarItem = (tarefa, status, indice) => {
-    const item = document.createElement('div');
-    item.classList.add('tasklist-item');
-    item.innerHTML = `
-        <input type="checkbox" name="checkbox" id="checkbox" onclick="${status}" class="checkbox-input">
-        <span class="task-name">${tarefa}</span>
-    `;
-    document.getElementById('todo-lists').appendChild(item);
+        const todo = { 
+            content: inputTask.value,
+            done: false,
+            createdAt: new Date().getTime()
+        }
+
+        todos.push(todo);
+
+        localStorage.setItem('todos', JSON.stringify(todos));
+
+        e.target.reset();
+
+        displayTodos();
+	});
+
+    displayTodos();
+});
+
+function displayTodos() {
+    const todoLists = document.querySelector('#todo-lists');
+
+    todoLists.innerHTML = ''
+
+    todos.forEach(todo => {
+        const todoItem = document.createElement('div');
+        todoItem.classList.add('tasklist-item');
+
+        const input = document.createElement('input');
+        const span = document.createElement('span');
+        const actions = document.createElement('div');
+        const edit = document.createElement('button');
+        const deleteButton = document.createElement('button');
+
+        input.type = 'checkbox';
+        input.checked = todo.done;
+        input.classList.add('checkbox-input');
+        actions.classList.add('actions');
+        edit.classList.add('edit');
+        deleteButton.classList.add('delete');
+
+        span.innerHTML = `
+            <input 
+                type="text" 
+                value="${todo.content}" 
+                class="task-name" 
+                style="background-color:rgba(0, 0, 0, 0)"
+                readonly
+            />`;
+        edit.innerHTML = 'Editar';
+        deleteButton.innerHTML = 'Deletar';
+
+        todoItem.appendChild(input);
+        todoItem.appendChild(span);
+        actions.appendChild(edit);
+        actions.appendChild(deleteButton);
+        todoItem.appendChild(actions);
+    
+        todoLists.appendChild(todoItem);
+
+        if (todo.done) {
+            todoItem.classList.add('done');
+        }
+
+        input.addEventListener('click', e => {
+            todo.done = e.target.checked;
+            localStorage.setItem('todos', JSON.stringify(todos));
+
+            if (todo.done) {
+                todoItem.classList.add('done');
+            } else {
+                todoItem.classList.remove('done');
+            }
+
+            displayTodos();
+        });
+
+        edit.addEventListener('click', e => {
+            const input = span.querySelector('input');
+            input.removeAttribute('readonly');
+            input.focus();
+            input.addEventListener('blur', e => {
+                input.setAttribute('readonly', true);
+                todo.content = e.target.value;
+                localStorage.setItem('todos', JSON.stringify(todos));
+                displayTodos();
+            });
+        });
+
+        deleteButton.addEventListener('click', e => {
+            todos = todos.filter(t => t != todo);
+            localStorage.setItem('todos', JSON.stringify(todos));
+            displayTodos();
+        })
+
+    });
 }
-
-const limparTarefas = () => {
-    const todoList = document.getElementById('todo-lists');
-    while (todoList.firstChild) {
-        todoList.removeChild(todoList.lastChild);
-    }
-}
-
-const atualizarTela = () => {
-    limparTarefas();
-    const banco = getBanco(); 
-    banco.forEach ( (item, indice) => criarItem (item.tarefa, item.status, indice));
-}
-
-const inserirItem = (evento) => {
-    const tecla = evento.key;
-    const texto = evento.target.value;
-    if (tecla === 'Enter'){
-        const banco = getBanco();
-        banco.push ({'tarefa': texto, 'status': ''});
-        setBanco(banco);
-        atualizarTela();
-        evento.target.value = '';
-    }
-}
-
-const removerItem = (indice) => {
-    const banco = getBanco();
-    banco.splice (indice, 1);
-    setBanco(banco);
-    atualizarTela();
-}
-
-const atualizarItem = (indice) => {
-    const banco = getBanco();
-    banco[indice].status = banco[indice].status === '' ? 'checked' : '';
-    setBanco(banco);
-    atualizarTela();
-}
-
-const clickItem = (evento) => {
-    const elemento = evento.target;
-    console.log (elemento.type);
-    if (elemento.type === 'button') {
-        const indice = elemento.dataset.indice;
-        removerItem (indice);
-    }else if (elemento.type === 'checkbox') {
-        const indice = elemento.dataset.indice;
-        atualizarItem (indice);
-    }
-}
-
-document.getElementById('taskAdd').addEventListener('keypress', inserirItem);
-document.getElementById('todo-lists').addEventListener('click', clickItem);
-
-atualizarTela();
